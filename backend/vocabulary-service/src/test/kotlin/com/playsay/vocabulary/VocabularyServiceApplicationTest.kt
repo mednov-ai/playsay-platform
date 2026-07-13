4429ceb80cac4efcbd04b046263194a4f5d80237
cbrgn <e.mednov@gmail.com>
2026-07-13 12:37:13 +0300

    fix: always migrate vocabulary schema

--- backend/vocabulary-service/src/test/kotlin/com/playsay/vocabulary/VocabularyServiceApplicationTest.kt
+++ backend/vocabulary-service/src/test/kotlin/com/playsay/vocabulary/VocabularyServiceApplicationTest.kt
@@ -1,18 +1,42 @@
 package com.playsay.vocabulary
 
+import javax.sql.DataSource
+import liquibase.integration.spring.SpringLiquibase
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.core.JdbcTemplate
 
 @SpringBootTest(
     properties = [
-        "spring.datasource.url=jdbc:h2:mem:vocabulary;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.url=jdbc:h2:mem:vocabulary;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;DB_CLOSE_DELAY=-1",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
         "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.jpa.hibernate.ddl-auto=create-drop",
-        "spring.liquibase.enabled=false",
+        "spring.jpa.hibernate.ddl-auto=none",
+        "spring.liquibase.enabled=true",
         "spring.security.oauth2.resourceserver.jwt.jwk-set-uri=http://localhost/unused-jwks",
     ],
 )
-class VocabularyServiceApplicationTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class VocabularyServiceApplicationTest @Autowired constructor(
+    private val jdbcTemplate: JdbcTemplate,
+    private val dataSource: DataSource,
+) {
+    @BeforeAll
+    fun migrateDatabase() {
+        SpringLiquibase().apply {
+            this.dataSource = this@VocabularyServiceApplicationTest.dataSource
+            changeLog = "classpath:db/changelog/db.changelog-master.xml"
+        }.afterPropertiesSet()
+    }
+
     @Test
-    fun contextLoads() = Unit
+    fun `liquibase creates vocabulary tables`() {
+        assertEquals(0, jdbcTemplate.queryForObject("select count(*) from vocabulary_entries", Long::class.java))
+        assertEquals(0, jdbcTemplate.queryForObject("select count(*) from vocabulary_occurrences", Long::class.java))
+    }
 }
